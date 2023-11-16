136b6e8e6258485de189c0172881fbbe6a290c2b
Андрей Епифанов <epifanovmd@gmail.com>
2023-11-16 21:45:58 +0300

    fix

--- template/src/common/helpers/index.ts
+++ template/src/common/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './componentProps';
 export * from './compound.helper';
+export * from './mergeRefs';
 export * from './platform.helper';
--- template/src/components/ui/carousel/Carousel.tsx
+++ template/src/components/ui/carousel/Carousel.tsx
@@ -1,10 +1,19 @@
-import React, {FC, memo, useCallback, useMemo, useRef, useState} from 'react';
-import {Animated, ListRenderItemInfo} from 'react-native';
-import {FlexView} from '../../elements';
-import {CarouselItem} from './CarouselItem';
+import React, {
+  FC,
+  forwardRef,
+  memo,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import {ListRenderItemInfo} from 'react-native';
+import {Col, Row} from '../../elements';
 import {LayoutChangeEvent} from 'react-native/Libraries/Types/CoreEventTypes';
 import {FlatListProps} from 'react-native/Libraries/Lists/FlatList';
 import {Touchable} from '../touchable';
+import {FlatList, GestureHandlerRootView} from 'react-native-gesture-handler';
+import {mergeRefs} from '../../../common';
 
 export interface ICarouselProps<T = any>
   extends Omit<
@@ -18,6 +27,8 @@ export interface ICarouselProps<T = any>
   width?: number;
   height?: number;
   onPress?: () => void;
+  overflowWidth?: number;
+  separateWidth?: number;
 }
 
 export interface CarouselFC<P = any> {
@@ -25,56 +36,88 @@ export interface CarouselFC<P = any> {
 }
 
 export const Carousel: CarouselFC = memo(
-  ({renderItem, data, height, width, onPress, ...rest}) => {
-    const [_width, setWidth] = useState(0);
-    const scrollX = useRef(new Animated.Value(0)).current;
-
-    const _renderItem = useCallback(
-      (item: ListRenderItemInfo<any>) => {
-        return (
-          <CarouselItem width={_width} height={height}>
-            <Touchable flex={1} onPress={onPress} disabled={!onPress}>
-              {renderItem?.(item)}
-            </Touchable>
-          </CarouselItem>
-        );
+  forwardRef<FlatList, ICarouselProps>(
+    (
+      {
+        renderItem,
+        data = [],
+        overflowWidth = 16,
+        separateWidth = 1,
+        onPress,
+        ...rest
       },
-      [renderItem, height, onPress, _width],
-    );
+      ref,
+    ) => {
+      const flatListRef = useRef<FlatList>(null);
+      const [width, setWidth] = useState(0);
 
-    const onLayout = useCallback((event: LayoutChangeEvent) => {
-      setWidth(event.nativeEvent.layout.width);
-    }, []);
+      useEffect(() => {
+        if (flatListRef.current && data && data?.length > 0) {
+          flatListRef.current.scrollToOffset({offset: 0});
+        }
+      }, [data]);
 
-    const renderEmpty = useMemo(
-      () =>
-        renderItem ? (
-          <CarouselItem width={_width} height={height}>
-            <Touchable flex={1} onPress={onPress} disabled={!onPress}>
-              {renderItem?.()}
-            </Touchable>
-          </CarouselItem>
-        ) : undefined,
-      [_width, height, onPress, renderItem],
-    );
+      const itemLength = width - overflowWidth; // Item is a square. Therefore, its height and width are of the same length.
+      const emptyItemLength = (width - itemLength) / 2;
 
-    return (
-      <FlexView onLayout={onLayout} height={height} width={width}>
-        <Animated.FlatList<any>
-          data={data}
-          renderItem={_renderItem}
-          horizontal={true}
-          pagingEnabled={true}
-          {...rest}
-          ListEmptyComponent={renderEmpty}
-          scrollEventThrottle={32}
-          onScroll={Animated.event(
-            [{nativeEvent: {contentOffset: {x: scrollX}}}],
-            {useNativeDriver: true},
-          )}
-          showsHorizontalScrollIndicator={false}
-        />
-      </FlexView>
-    );
-  },
+      const _renderItem = useCallback(
+        (item: ListRenderItemInfo<any>) => {
+          return (
+            <Col
+              mh={separateWidth}
+              width={itemLength - separateWidth * 2}
+              height={'auto'}>
+              <Touchable
+                flexShrink={1}
+                overflow={'hidden'}
+                leftRadius={item.index === 0 ? 10 : 0}
+                rightRadius={item.index + 1 === data?.length ? 10 : 0}
+                onPress={onPress}>
+                {renderItem?.(item)}
+              </Touchable>
+            </Col>
+          );
+        },
+        [data?.length, itemLength, separateWidth, onPress, renderItem],
+      );
+
+      const onLayout = useCallback(
+        (event: LayoutChangeEvent) => {
+          if (!width) {
+            setWidth(event.nativeEvent.layout.width);
+          }
+        },
+        [width],
+      );
+
+      const EmptyHorizontalItem = useCallback(
+        () => <Col style={{width: emptyItemLength}} />,
+        [emptyItemLength],
+      );
+
+      return (
+        <Row onLayout={onLayout} flex={1}>
+          <GestureHandlerRootView>
+            <FlatList
+              ref={mergeRefs([ref, flatListRef])}
+              data={data}
+              horizontal={true}
+              ListHeaderComponent={EmptyHorizontalItem}
+              ListFooterComponent={EmptyHorizontalItem}
+              showsHorizontalScrollIndicator={false}
+              bounces={false}
+              decelerationRate={0.95}
+              renderToHardwareTextureAndroid
+              snapToInterval={itemLength + separateWidth}
+              snapToAlignment="start"
+              keyExtractor={(item, index) => `${index}`}
+              {...rest}
+              renderItem={_renderItem}
+              ListEmptyComponent={_renderItem}
+            />
+          </GestureHandlerRootView>
+        </Row>
+      );
+    },
+  ),
 );
